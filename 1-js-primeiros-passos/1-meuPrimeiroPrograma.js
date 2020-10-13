b076d6dfedb92fa3322eaabd68ce754799db6141
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-10-13 18:22:05 -0300

    declarando variáveis, imprimindo valores e utilizando operadores matemáticos

---
+++ 1-js-primeiros-passos/1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+//Aula 1 
+console.log("Meu primeiro programa");
+
+const idade = 26;
+
+const idadeSomada = idade+2;
+console.log("resultado da const idadeSomada = ", idadeSomada);
+
+//utilizando operadores matemáticos
+console.log("idade=", idade);
+console.log("idade=", idade+2);
+console.log("idade=", idade-2);
+console.log("idade=", idade/2);
