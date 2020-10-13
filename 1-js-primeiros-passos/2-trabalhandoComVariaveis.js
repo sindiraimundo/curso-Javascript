502cec5b9ebec3734a82b2af108f810f3553922d
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-10-13 18:48:05 -0300

    trabalhando com variáveis, boas práticas

---
+++ 1-js-primeiros-passos/2-trabalhandoComVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com variáveis");
+//obs:variaveis sempre com o mesmo padrão.
+
+//BOAS PRÁTICAS
+const nome = "Lais";
+const Sobrenome = "Silva";
+const Idade = 30; 
+const anoDeNascimento = 1993;
+//não é boa prática esse tipo de declaração de variáveis é preciso ter um padrão de declaração.
+
+console.log(nome);
+console.log(Sobrenome);
+console.log(Idade);
+console.log(anoDeNascimento);
