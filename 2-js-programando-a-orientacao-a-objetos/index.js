e6516a8fdee95d376e18267611ee5678d584e8ca
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-10-14 20:03:55 -0300

    criando modulos importando e exportando, adicionando type: module no package.json

--- 2-js-programando-a-orientacao-a-objetos/index.js
+++ 2-js-programando-a-orientacao-a-objetos/index.js
@@ -1,29 +1,5 @@
-class Cliente{
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    //#saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-        this._saldo += valor;            
-    }
-
-}
-
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente(); 
 cliente1.nome = "Ricardo";
@@ -44,5 +20,3 @@ contaCorreteRicardo.depositar(100);
 const valorSacado = contaCorreteRicardo.sacar(50);
 console.log(valorSacado);
 console.log(contaCorreteRicardo);
-
-
