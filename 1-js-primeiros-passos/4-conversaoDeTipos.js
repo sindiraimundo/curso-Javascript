1d6fe41a20fe391f4b079e40568f64c17bc94e40
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-10-13 21:28:57 -0300

    realizando conversão de string para número com parseInt

---
+++ 1-js-primeiros-passos/4-conversaoDeTipos.js
@@ -0,0 +1,16 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2020);//resultado ano2020.
+console.log("2" + "2"); // apenas junta as strings resultado=22.a soma nesse caso está concatenando.
+
+console.log(parseInt("2")+parseInt("2"))// utilizando parseInt para fazer a conversão do texto "2", para número 2;resultado = 4 como número;
+console.log("10"/"2");// neste caso de divisão o js faz a conversão implicitamente sem que eu precise utilizar o parseInt.reultado = 5
+//parseInt utilizamos se queremos retornar um inteiro.
+console.log("Ricardo" / "2");//retorna NaN( Not a number)não é um número.
+
+console.log(6.5);//em números o js trabalha com . flutuante quando trabalhamos com casas decimais; resultado=6.5;
+console.log(6,5);//resultado = 6 5 (acrescenta um espação no lugar da vírgula, que é interpretado como caractere especial);
+
+//parseFloat forçar o número a ter casa decimal
+
+
