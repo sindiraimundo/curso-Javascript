fdfa950b6607ba2adcc999e487e2b7284b0a2a53
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-10-13 22:12:55 -0300

    realizando atribuição de variaveis

---
+++ 1-js-primeiros-passos/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,22 @@
+console.log("Trabalhando com Atribuição de variáveis");
+
+
+
+const idade = 29;
+const primeiroNome = "Ricardo";
+const sobrenome = "Bugan";
+
+console.log(primeiroNome  + sobrenome);
+console.log(primeiroNome , sobrenome);
+//console.log(nome + " " + sobrenome);
+
+console.log(`Meu nome é ${primeiroNome } ${sobrenome}`);
+
+primeiroNome  = primeiroNome  + sobrenome;//aqui estamos subscrevendo a variável primeiroNome, atribuindo um novo valor;
+const nomeCompleto = primeiroNome  + sobrenome // armazenando nome e sobrenome em uma variável constante;
+console.log("nomeCompleto", nomeCompleto);
+
+//let idade;//declarando variável
+//idade = 26;// atribuindo valor; resultado=26.
+//idade = idade +1; //reatribui o valor de idade; resultado = 27.
+
