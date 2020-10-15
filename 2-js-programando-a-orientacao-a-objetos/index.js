6ab5fbc30b11cc0f1324b3a01cee421013dba7e8
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-10-15 13:38:02 -0300

    instanciando novo cliente

--- 2-js-programando-a-orientacao-a-objetos/index.js
+++ 2-js-programando-a-orientacao-a-objetos/index.js
@@ -5,9 +5,9 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233399;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233399;
+// const cliente2 = new Cliente();
+// cliente2.nome = "Alice";
+// cliente2.cpf = 88822233399;
 
 
 const contaCorreteRicardo = new ContaCorrente();
@@ -16,7 +16,9 @@ contaCorreteRicardo.cliente = cliente1;
 contaCorreteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = new Cliente();
+conta2.cliente.nome = "Alice";
+conta2.cliente.cpf = 88822233399;
 conta2.agencia = 102;
 
 let valor = 200;                        
